f7a75fedf00911ae1c240a53c3b97df312a443d8
Erwin Lejeune <erwin.lejeune@cm-robotics.com>
2022-04-10 06:48:02 +0200

    Test communication with/without spinning

--- test/node_communication_test.cpp
+++ test/node_communication_test.cpp
@@ -5,7 +5,7 @@
 #include "cmr_tests_utils/basic_subscriber_node_test.hpp"
 #include "std_msgs/msg/int32.hpp"
 
-TEST(NodeCommunicationTest, spinning_test)
+TEST(NodeCommunicationTest, simple_pub_sub_communication)
 {
   rclcpp::init(0, nullptr);
 
@@ -18,23 +18,36 @@ TEST(NodeCommunicationTest, spinning_test)
   basic_node.cancel_spin();
 }
 
-TEST(NodeCommunicationTest, is_node_spinning)
+TEST(NodeCommunicationTest, simple_spinning_pub_sub_datastream)
 {
   rclcpp::init(0, nullptr);
 
   auto sub_node = cmr_tests_utils::BasicSubscriberNodeTest<std_msgs::msg::Int32>("sub_test_node", "test_topic");
   auto pub_node = cmr_tests_utils::BasicPublisherNodeTest<std_msgs::msg::Int32>("pub_test_node", "test_topic", false, 100);
-  sub_node.spin_in_new_thread();
-  pub_node.spin_in_new_thread();
+  EXPECT_FALSE(sub_node.get_is_spinning());
+  EXPECT_FALSE(pub_node.get_is_spinning());
   EXPECT_EQ(sub_node.get_received_msg(), nullptr);
 
+  // Publish Data without Spinning
   std_msgs::msg::Int32 msg;
   msg.data = 42;
   pub_node.publish(msg);
   sleep(2);
+
+  // Subscriber shouldn't have received anything
+  EXPECT_TRUE(!sub_node.get_received_msg());
+
+  // Spin both nodes
+  sub_node.spin_in_new_thread();
+  pub_node.spin_in_new_thread();
+
+  // We expect the data to be received now
+  EXPECT_TRUE(pub_node.get_is_spinning());
+  EXPECT_TRUE(sub_node.get_is_spinning());
   EXPECT_FALSE(!sub_node.get_received_msg());
   EXPECT_EQ(sub_node.get_received_msg()->data, 42);
-
+  
+  // Publish a message when both nodes are spinning
   msg.data = 1337;
   pub_node.publish(msg);
   sleep(2);

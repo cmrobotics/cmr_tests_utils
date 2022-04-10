0b43c3625dfcd62323e50c0901bdde4861f95bd4
Erwin Lejeune <erwin.lejeune@cm-robotics.com>
2022-04-10 16:08:12 +0200

    Add cancel spin in destructor

--- include/cmr_tests_utils/basic_node_test.hpp
+++ include/cmr_tests_utils/basic_node_test.hpp
@@ -11,22 +11,30 @@ class BasicNodeTest: public rclcpp::Node
   public:
 
   BasicNodeTest(std::string node_name): Node(node_name) {}
+  ~BasicNodeTest()
+  {
+    if (spinner_) cancel_spin();
+  }
 
   void spin_in_new_thread()
   {
-    if (spin_thread_)
+    if (spinner_)
     {
       RCLCPP_WARN(get_logger(), "Tried to spin in new thread but node is already spinning, ignoring...");
       return;
     }
-    spin_thread_ = std::make_shared<std::thread>(&BasicNodeTest::spin_, this);
+    if (!rclcpp::ok()) {
+      RCLCPP_ERROR(get_logger(), "Failed to spin node, rclcpp is not healthy.");
+      return;
+    }
+    spinner_ = std::make_shared<std::thread>(&BasicNodeTest::spin_, this);
     is_spinning_.store(true);
   }
 
   void cancel_spin()
   {
-    spin_thread_->join();
-    spin_thread_.reset();
+    spinner_->join();
+    spinner_.reset();
     is_spinning_.store(false);
   }
 
@@ -42,8 +50,8 @@ class BasicNodeTest: public rclcpp::Node
     rclcpp::spin(get_node_base_interface());
   }
 
-  std::shared_ptr<std::thread> spin_thread_;
-  std::atomic<bool> is_spinning_;
+  std::shared_ptr<std::thread> spinner_;
+  std::atomic<bool> is_spinning_ = false;
   
 };
 

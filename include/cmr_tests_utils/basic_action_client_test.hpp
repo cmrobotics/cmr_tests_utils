8b7e2a35ab314731a1650db3f8006720acbf41d0
DIEUDONNE Clara <dieudonne.cl@gmail.com>
2022-06-29 10:07:53 +0200

    error expected class-name

--- include/cmr_tests_utils/basic_action_client_test.hpp
+++ include/cmr_tests_utils/basic_action_client_test.hpp
@@ -1,13 +1,11 @@
 #pragma once
 
-using namespace cmr_clients_utils; 
-
-class BasicActionClient;
+#include "cmr_clients_utils/basic_action_client.hpp"
 
 namespace cmr_tests_utils {
 
 template<class ActionT>
-class BasicActionClientTest : public BasicActionClient {
+class BasicActionClientTest : public cmr_clients_utils::BasicActionClient {
 
 };
 

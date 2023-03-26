dfd3b3263954ec7fac2db02188ba6b0f83152e6d
arekin19 <arekin19@gmail.com>
2023-03-26 12:42:01 +0200

    fix obsolete include (warning)

--- include/cmr_tests_utils/basic_tf_listener_node_test.hpp
+++ include/cmr_tests_utils/basic_tf_listener_node_test.hpp
@@ -6,7 +6,7 @@
 #include <geometry_msgs/msg/transform_stamped.hpp>
 #include <geometry_msgs/msg/pose_stamped.hpp>
 #include <tf2_ros/transform_broadcaster.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_listener.h>
 #include <tf2/utils.h>
